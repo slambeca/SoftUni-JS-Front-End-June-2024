15bfa3808a1aebb6f19240c6a98c68da06d48000
Borislav Ivanov <slambeca@abv.bg>
2024-06-27 14:46:40 +0300

    Add files via upload

---
+++ 1.2 JS Syntax Fundamentals - Exercise/vacation.js
@@ -0,0 +1,51 @@
+function vacation(countPeople, type, day) {
+    // const isFriday = day === 'Friday';
+    // const isStudentGroup = type === 'Students';
+    // const isBusinessGroup = type === 'Business';
+    // const isRegularGroup = type === 'Regular';
+
+    let ticketPrice;
+    let totalTicketPrice;
+
+
+    if (day === 'Friday') {
+        if (type === 'Students') {
+            ticketPrice = 8.45;
+        } else if (type === 'Business') {
+            ticketPrice = 10.90;
+        } else if (type === 'Regular') {
+            ticketPrice = 15;
+        }
+    } else if (day === 'Saturday') {
+        if (type === 'Students') {
+            ticketPrice = 9.80;
+        } else if (type === 'Business') {
+            ticketPrice = 15.60;
+        } else if (type === 'Regular') {
+            ticketPrice = 20;
+        }
+    } else if (day === 'Sunday') {
+        if (type === 'Students') {
+            ticketPrice = 10.46;
+        } else if (type === 'Business') {
+            ticketPrice = 16;
+        } else if (type === 'Regular') {
+            ticketPrice = 22.50;
+        }
+    }
+
+    totalTicketPrice = countPeople * ticketPrice
+
+    if (type === 'Students' && countPeople >= 30) {
+        totalTicketPrice *= 0.85;
+    } else if (type === 'Regular' && countPeople >= 10 && countPeople <= 20) {
+        totalTicketPrice *= 0.95;
+    } else if (type === 'Business' && countPeople >= 100) {
+        totalTicketPrice = totalTicketPrice - 10 * ticketPrice;
+    }
+    console.log(`Total price: ${totalTicketPrice.toFixed(2)}`);
+}
+
+
+vacation(30, "Students", "Sunday");
+vacation(40, 'Regular', 'Saturday');
