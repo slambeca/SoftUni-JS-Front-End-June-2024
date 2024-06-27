b50ed6c8fb3970bf65fe58fcd42bcb63afe1088b
Borislav Ivanov <slambeca@abv.bg>
2024-06-27 14:57:21 +0300

    Second resolution added

--- 1.2 JS Syntax Fundamentals - Exercise/vacation.js
+++ 1.2 JS Syntax Fundamentals - Exercise/vacation.js
@@ -49,3 +49,59 @@ function vacation(countPeople, type, day) {
 
 vacation(30, "Students", "Sunday");
 vacation(40, 'Regular', 'Saturday');
+
+
+// Variant 2
+function vacation(countPeople, type, day) {
+    const isFriday = day === 'Friday';
+    const isSaturday = day === 'Saturday';
+    const isSunday = day === 'Sunday';
+
+    const isStudentsGroup = type === 'Students';
+    const isBusinessGroup = type === 'Business';
+    const isRegularGroup = type === 'Regular';
+
+    let ticketPrice;
+    let totalTicketPrice;
+
+
+    if (isFriday) {
+        if (isStudentsGroup) {
+            ticketPrice = 8.45;
+        } else if (isBusinessGroup) {
+            ticketPrice = 10.90;
+        } else if (isRegularGroup) {
+            ticketPrice = 15;
+        }
+    } else if (isSaturday) {
+        if (isStudentsGroup) {
+            ticketPrice = 9.80;
+        } else if (isBusinessGroup) {
+            ticketPrice = 15.60;
+        } else if (isRegularGroup) {
+            ticketPrice = 20;
+        }
+    } else if (isSunday) {
+        if (isStudentsGroup) {
+            ticketPrice = 10.46;
+        } else if (isBusinessGroup) {
+            ticketPrice = 16;
+        } else if (isRegularGroup) {
+            ticketPrice = 22.50;
+        }
+    }
+
+    totalTicketPrice = countPeople * ticketPrice
+
+    if (type === 'Students' && countPeople >= 30) {
+        totalTicketPrice *= 0.85;
+    } else if (type === 'Regular' && countPeople >= 10 && countPeople <= 20) {
+        totalTicketPrice *= 0.95;
+    } else if (type === 'Business' && countPeople >= 100) {
+        totalTicketPrice = totalTicketPrice - 10 * ticketPrice;
+    }
+    console.log(`Total price: ${totalTicketPrice.toFixed(2)}`);
+}
+
+vacation(30, "Students", "Sunday");
+vacation(40, 'Regular', 'Saturday');
