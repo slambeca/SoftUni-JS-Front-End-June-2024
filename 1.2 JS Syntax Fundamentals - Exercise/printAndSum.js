e46d8948af984f5dfa3c147587de3fe751a82a7f
Borislav Ivanov <slambeca@abv.bg>
2024-07-01 14:45:13 +0300

    Second resolution added

--- 1.2 JS Syntax Fundamentals - Exercise/printAndSum.js
+++ 1.2 JS Syntax Fundamentals - Exercise/printAndSum.js
@@ -1,13 +1,29 @@
 function printAndSum(firstNum, secondNum) {
     let sum = 0;
 
-    for (let i = firstNum; i <= secondNum; i++) {
+    for(let i = firstNum; i <= secondNum; i++) {
         sum += i;
         process.stdout.write(i.toString() + ' ');
     }
 
     console.log('\n' + `Sum: ${sum}`)
 
+}
+
+printAndSum(50, 60);
+
+// Variant 2
+function printAndSum(firstNum, secondNum) {
+    let output = "";
+    let sum = 0;
+
+    for(let currentNumber=firstNum; currentNumber <= secondNum; currentNumber += 1) {
+        output += `${currentNumber} `;
+        sum += currentNumber
+    }
+
+    console.log(output.trim());
+    console.log(`Sum: ${sum}`);
 }
 
 printAndSum(50, 60);
