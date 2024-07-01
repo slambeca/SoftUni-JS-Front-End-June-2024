3583325fc53b10bae413d0e8543d3b941f341af4
Borislav Ivanov <slambeca@abv.bg>
2024-07-01 15:24:19 +0300

    Second resolution added with while loop and modulo operator

--- 1.2 JS Syntax Fundamentals - Exercise/sumDigits.js
+++ 1.2 JS Syntax Fundamentals - Exercise/sumDigits.js
@@ -10,3 +10,18 @@ function sumDigits(number) {
 }
 
 sumDigits(245678); // should return 32
+
+// Variant 2
+function sumDigitsWhileLoop(number) {
+    let currentNumber = number;
+    let sum = 0;
+
+    while(currentNumber > 0) {
+        sum += currentNumber % 10;
+        currentNumber = parseInt(currentNumber / 10);
+    }
+
+    console.log(sum);
+}
+
+sumDigitsWhileLoop(245678);
