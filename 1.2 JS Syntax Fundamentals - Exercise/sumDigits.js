9033358f7dc322f7c292d4254f2e57f2f55c8be6
Borislav Ivanov <slambeca@abv.bg>
2024-07-01 15:14:11 +0300

    Add files via upload

---
+++ 1.2 JS Syntax Fundamentals - Exercise/sumDigits.js
@@ -0,0 +1,12 @@
+function sumDigits(number) {
+    const numberAsString = number.toString();
+    let sum = 0;
+
+    for(let i = 0; i < numberAsString.length; i++) {
+        sum += Number(numberAsString[i]);
+    }
+
+    console.log(sum);
+}
+
+sumDigits(245678); // should return 32
