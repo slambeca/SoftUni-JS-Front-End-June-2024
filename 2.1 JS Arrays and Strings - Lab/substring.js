bb2d4cc670b8be7892cf8349a367292467a35ff5
Borislav Ivanov <slambeca@abv.bg>
2024-07-03 21:17:30 +0300

    Add files via upload

---
+++ 2.1 JS Arrays and Strings - Lab/substring.js
@@ -0,0 +1,6 @@
+function substringFromAString(string, startIndex, count) {
+    console.log(string.substring(startIndex, startIndex+count));
+}
+
+substringFromAString('ASentence', 1, 8);
+substringFromAString('SkipWord', 4, 7);
