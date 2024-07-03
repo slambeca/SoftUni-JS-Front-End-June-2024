0a4dd2bccbeac26b7aa30d7451f204f098dc7e00
Borislav Ivanov <slambeca@abv.bg>
2024-07-03 17:53:01 +0300

    Add files via upload

---
+++ 2.1 JS Arrays and Strings - Lab/sumFirstAndLast.js
@@ -0,0 +1,18 @@
+function sumFirstAndLast(numbersArray) {
+    let sum = 0;
+
+    let firstElement = numbersArray[0];
+    let lastElement = numbersArray[numbersArray.length - 1];
+    sum = firstElement + lastElement;
+
+    console.log(sum);
+}
+
+sumFirstAndLast([10, 17, 22, 33])
+
+// Variant 2
+function sumFirstAndLast(numbersArray) {
+    console.log(numbersArray[0] + numbersArray[numbersArray.length - 1]);
+}
+
+sumFirstAndLast([10, 20, 30])
