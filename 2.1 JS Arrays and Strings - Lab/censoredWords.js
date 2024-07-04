86ce15bcfcac22fcf9b653317a8aebd2c01c5dd5
Borislav Ivanov <slambeca@abv.bg>
2024-07-04 17:03:56 +0300

    Second resolution added

--- 2.1 JS Arrays and Strings - Lab/censoredWords.js
+++ 2.1 JS Arrays and Strings - Lab/censoredWords.js
@@ -4,3 +4,15 @@ function censoredWords(text, word) {
 
 censoredWords('A small sentence with some words', 'small');
 censoredWords('Find the hidden word', 'hidden');
+
+// Variant 2
+function solveWithoutReplaceAll(text, word) {
+    while(text.includes(word)) {
+        text = text.replace(word, '*'.repeat(word.length))
+    }
+
+    console.log(text);
+}
+
+solveWithoutReplaceAll('A small sentence with small words', 'small');
+solveWithoutReplaceAll('Find the hidden word', 'hidden');
