591dba78a664022cd467f902d24255dd9019b598
Borislav Ivanov <slambeca@abv.bg>
2024-07-05 15:44:45 +0300

    Second resolution added without for loop

--- 2.2 JS Arrays and Strings - Exercise/arrayRotation.js
+++ 2.2 JS Arrays and Strings - Exercise/arrayRotation.js
@@ -12,3 +12,18 @@ function arrayRotation(array, count) {
 arrayRotation([51, 47, 32, 61, 21], 2);
 arrayRotation([32, 21, 61, 1], 4);
 arrayRotation([2, 4, 15, 31], 5);
+
+// Variant 2 without for loop
+function arrayRotation(array=[], rotations=Number) {
+    let currentArray = array;
+    let cutOffIndex = rotations % currentArray.length;
+
+    leftSideArray = currentArray.slice(0, cutOffIndex);
+    rightSideArray = currentArray.slice(cutOffIndex);
+
+    console.log(rightSideArray.concat(leftSideArray).join(' '));
+}
+
+arrayRotation([51, 47, 32, 61, 21], 2);
+arrayRotation([32, 21, 61, 1], 4);
+arrayRotation([2, 4, 15, 31], 5);
