8bc32992bbd6a58b1613c29c5c7d5204a41ebc15
Borislav Ivanov <slambeca@abv.bg>
2024-07-13 14:01:09 +0300

    Delete 3.1 Functions and Statements directory

--- 3.1 Functions and Statements/mathPower.js
+++
@@ -1,6 +0,0 @@
-function calculateMathPower(number, power) {
-    console.log(number ** power);
-}
-
-calculateMathPower(2, 8);
-calculateMathPower(3, 4);
--- 3.1 Functions and Statements/repeatString.js
+++
@@ -1,7 +0,0 @@
-function repeatString(string='', count=Number) {
-    let result = string.repeat(count)
-
-    return result
-}
-
-console.log(repeatString('abc', 3));
