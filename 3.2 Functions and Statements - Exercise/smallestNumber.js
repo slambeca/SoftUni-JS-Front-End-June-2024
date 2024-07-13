7a9abb8e67199a1844c11fb2e1dc49b62b2359a7
Borislav Ivanov <slambeca@abv.bg>
2024-07-13 14:02:03 +0300

    Add files via upload

---
+++ 3.2 Functions and Statements - Exercise/smallestNumber.js
@@ -0,0 +1,11 @@
+function findSmallestNumber(firstNum=Number, secondNum=Number, thirdNum=Number) {
+    const smallestNumber = Math.min(firstNum, secondNum, thirdNum);
+
+    console.log(smallestNumber);
+}
+
+
+findSmallestNumber(2, 5, 3);
+findSmallestNumber(600, 342, 123);
+findSmallestNumber(25, 21,4);
+findSmallestNumber(2, 2, 2);
