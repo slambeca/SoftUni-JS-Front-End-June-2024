630a22776b2be042fade5a77bc4fe61c073ab9f9
Borislav Ivanov <slambeca@abv.bg>
2024-07-13 14:05:30 +0300

    Second resolution was added

--- 3.2 Functions and Statements - Exercise/smallestNumber.js
+++ 3.2 Functions and Statements - Exercise/smallestNumber.js
@@ -9,3 +9,20 @@ findSmallestNumber(2, 5, 3);
 findSmallestNumber(600, 342, 123);
 findSmallestNumber(25, 21,4);
 findSmallestNumber(2, 2, 2);
+
+// Variant 2
+function findSmallestNumberNew(a, b, c) {
+    let smallestNumber = a;
+
+    if (b < smallestNumber) {
+        smallestNumber = b;
+    }
+
+    if (c < smallestNumber) {
+        smallestNumber = c;
+    }
+
+    console.log(smallestNumber);
+}
+
+findSmallestNumberNew(2, 10, 5);
