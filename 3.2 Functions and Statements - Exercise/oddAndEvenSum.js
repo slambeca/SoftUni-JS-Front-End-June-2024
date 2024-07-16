99231574c8ee334a71c7232a3091a90b335d5eea
Borislav Ivanov <slambeca@abv.bg>
2024-07-16 13:11:33 +0300

    Second resolution added with custom sum function and reduce()

--- 3.2 Functions and Statements - Exercise/oddAndEvenSum.js
+++ 3.2 Functions and Statements - Exercise/oddAndEvenSum.js
@@ -18,3 +18,32 @@ function printOddAndEvenSum(num=Number) {
 
 printOddAndEvenSum(1000435);
 printOddAndEvenSum(3495892137259234);
+
+// Variant 2
+
+function printOddAndEvenSum2(num=Number) {
+    function sum(array) {
+        return array.reduce((accumulator, currentValue) => accumulator + Number(currentValue), 0)
+    }
+
+    let oddNumbers = [];
+    let evenNumbers = [];
+
+    const numAsString = num.toString();
+
+    for (i = 0; i < numAsString.length; i++) {
+        if (numAsString[i] % 2 == 0) {
+            evenNumbers.push(numAsString[i]);
+        } else {
+            oddNumbers.push(numAsString[i]);
+        }
+    }
+
+    let sumOddNumbers = sum(oddNumbers);
+    let sumEvenNumbers = sum(evenNumbers);
+
+    console.log(`Odd sum = ${sumOddNumbers}, Even sum = ${sumEvenNumbers}`);
+}
+
+printOddAndEvenSum2(1000435);
+printOddAndEvenSum2(3495892137259234);
