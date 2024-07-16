9a75bf091dd88a7cbe3d39c9eafd19a6c464a243
Borislav Ivanov <slambeca@abv.bg>
2024-07-16 15:46:25 +0300

    With two resolutions, the second one is shorter

---
+++ 3.2 Functions and Statements - Exercise/nxNMatrix.js
@@ -0,0 +1,25 @@
+function printNxNMatrix(number) {
+    for (let i = 0; i < number; i++) {
+        let row = '';
+
+        for (let j = 0; j < number; j++) {
+            row += `${number} `;
+        }
+
+        console.log(row.trim());
+        
+    }
+}
+
+
+printNxNMatrix(7);
+
+
+// Variant 2
+function printMatrix(number) {
+    for (let i = 0; i < number; i++) {
+        console.log(`${number} `.repeat(number));
+    }
+}
+
+printMatrix(2)
