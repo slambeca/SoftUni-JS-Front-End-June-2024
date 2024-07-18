a6d128fa2288149271d50f73f71f0d03a0a40e1d
Borislav Ivanov <slambeca@abv.bg>
2024-07-18 20:17:56 +0300

    Add files via upload

---
+++ 4.1 JS Classes and Objects - Lab/cats.js
@@ -0,0 +1,21 @@
+function solve(input) {
+    class Cat {
+        constructor(name, age) {
+            this.name = name;
+            this.age = age;
+        }
+    
+        meow() {
+            console.log(`${this.name}, age ${this.age} says Meow`);
+        }
+    }
+
+    for (const entry of input) {
+        const [name, age] = entry.split(' ');
+
+        const cat = new Cat(name, parseInt(age));
+        cat.meow();
+    }
+}
+
+solve(['Mellow 2', 'Tom 5'])
