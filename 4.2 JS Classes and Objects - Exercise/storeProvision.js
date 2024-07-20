9af5ae7f593dc4d0cf609dc132004ab63b94cf57
Borislav Ivanov <slambeca@abv.bg>
2024-07-20 18:28:44 +0300

    The resolution with updated format

--- 4.2 JS Classes and Objects - Exercise/storeProvision.js
+++ 4.2 JS Classes and Objects - Exercise/storeProvision.js
@@ -1,16 +1,16 @@
 function storeProvision(stock, orderedProducts) {
     let allProducts = {};
 
-    for(let i=0; i<stock.length; i+=2) {
-        allProducts[stock[i]] = parseInt(stock[i+1]);
-        
+    for (let i = 0; i < stock.length; i += 2) {
+        allProducts[stock[i]] = parseInt(stock[i + 1]);
+
     }
 
-    for(let j=0; j<orderedProducts.length; j+=2) {
+    for (let j = 0; j < orderedProducts.length; j += 2) {
         const name = orderedProducts[j];
-        const quantity = parseInt(orderedProducts[j+1]);
+        const quantity = parseInt(orderedProducts[j + 1]);
 
-        if(allProducts[name]) {
+        if (allProducts[name]) {
             allProducts[name] += quantity;
         } else {
             allProducts[name] = quantity;
@@ -24,8 +24,8 @@ function storeProvision(stock, orderedProducts) {
 
 storeProvision([
     'Chips', '5', 'CocaCola', '9', 'Bananas', '14', 'Pasta', '4', 'Beer', '2'
-    ],
+],
     [
-    'Flour', '44', 'Oil', '12', 'Pasta', '7', 'Tomatoes', '70', 'Bananas', '30'
+        'Flour', '44', 'Oil', '12', 'Pasta', '7', 'Tomatoes', '70', 'Bananas', '30'
     ]
-    );
+);
