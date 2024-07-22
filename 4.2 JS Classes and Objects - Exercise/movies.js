26dc383e6f4cb29a45ebc37864797df342ebadb4
Borislav Ivanov <slambeca@abv.bg>
2024-07-22 17:55:26 +0300

    Add files via upload

---
+++ 4.2 JS Classes and Objects - Exercise/movies.js
@@ -0,0 +1,40 @@
+function manageMovies(commands) {
+    let movies = [];
+
+    for (currentMovie of commands) {
+        if (currentMovie.startsWith('addMovie')) {
+            let movieName = currentMovie.substring(9);
+            movies.push({ name: movieName});
+        } else if (currentMovie.includes(' directedBy ')) {
+            let [movieName, movieDirector] = currentMovie.split(' directedBy ');
+            let movie = movies.find(m => m.name === movieName);
+            if (movie) {
+                movie.director = movieDirector;
+            }
+        } else if (currentMovie.includes(' onDate ')) {
+            let [movieName, movieDate] = currentMovie.split(' onDate ');
+            let movie = movies.find(m => m.name === movieName) 
+            if (movie) {
+                movie.date = movieDate;
+            }
+        }
+    }
+
+    movies.forEach(movie => {
+        if (movie.name && movie.director && movie.date) {
+            console.log(JSON.stringify(movie));
+        }
+    });
+}
+
+manageMovies([
+    'addMovie Fast and Furious',
+    'addMovie Godfather',
+    'Inception directedBy Christopher Nolan',
+    'Godfather directedBy Francis Ford Coppola',
+    'Godfather onDate 29.07.2018',
+    'Fast and Furious onDate 30.07.2018',
+    'Batman onDate 01.08.2018',
+    'Fast and Furious directedBy Rob Cohen'
+]
+);
