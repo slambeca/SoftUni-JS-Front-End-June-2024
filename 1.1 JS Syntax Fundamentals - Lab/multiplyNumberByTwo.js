defc4efe14f1218f8f811374bbc8c6f3d1ccefc7
Borislav Ivanov <slambeca@abv.bg>
2024-07-22 17:07:19 +0300

    Update multiplyNumberByTwo.js

--- 1.1 JS Syntax Fundamentals - Lab/multiplyNumberByTwo.js
+++ 1.1 JS Syntax Fundamentals - Lab/multiplyNumberByTwo.js
@@ -2,10 +2,10 @@ function solve(num) {
     console.log(num * 2);
 }
 
-solve(4)
+solve(4);
 
 // Variant 2
 function solve(number) {
     let result = number * 2
-      console.log(result) 
+      console.log(result);
   }
