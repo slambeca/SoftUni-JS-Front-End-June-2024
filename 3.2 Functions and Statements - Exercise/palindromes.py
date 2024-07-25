604f979b3dec7c7f7ee839c30b81d65e6ac9984d
Borislav Ivanov <slambeca@abv.bg>
2024-07-25 18:28:48 +0300

    Update palindromes.py

--- 3.2 Functions and Statements - Exercise/palindromes.py
+++ 3.2 Functions and Statements - Exercise/palindromes.py
@@ -1,13 +1,13 @@
 def findPalindromes(array):
     for currentNumber in array:
-        number_as_string = str(currentNumber)
+        number_as_string = str(currentNumber);
 
-        reversed_number = number_as_string[::-1]
+        reversed_number = number_as_string[::-1];
 
         if number_as_string == reversed_number:
-            print('true')
+            print('true');
         else:
-            print('false')
+            print('false');
 
 
-findPalindromes([100,100,101,1,12321])
+findPalindromes([100,100,101,1,12321]);
