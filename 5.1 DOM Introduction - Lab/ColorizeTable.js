a0f0109765291eb14781da77038aa1763fde8c85
Borislav Ivanov <slambeca@abv.bg>
2024-07-26 18:58:19 +0300

    Third resolution added

--- 5.1 DOM Introduction - Lab/ColorizeTable.js
+++ 5.1 DOM Introduction - Lab/ColorizeTable.js
@@ -1,11 +1,11 @@
-// function colorize() {
-//     const tableElement = document.querySelector('table tbody');
-//     const tableRowElements = tableElement.children;
+function colorize() {
+    const tableElement = document.querySelector('table tbody');
+    const tableRowElements = tableElement.children;
 
-//     for (let i = 0; i < tableRowElements.length; i += 2) {
-//         tableRowElements[i].style.backgroundColor = 'teal';
-//     }
-// }
+    for (let i = 0; i < tableRowElements.length; i += 2) {
+        tableRowElements[i].style.backgroundColor = 'teal';
+    }
+}
 
 // Variant 2
 function colorize() {
