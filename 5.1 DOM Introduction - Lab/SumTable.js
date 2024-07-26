a28b585a380be7083dd58ef9bcd1048a7f691752
Borislav Ivanov <slambeca@abv.bg>
2024-07-26 19:11:04 +0300

    Add files via upload

---
+++ 5.1 DOM Introduction - Lab/SumTable.js
@@ -0,0 +1,19 @@
+function sumTable() {
+    const resultElement = document.getElementById('sum');
+
+    const trElements = document.querySelectorAll('table tbody tr');
+    let sum = 0;
+
+    for (const trElement of trElements) {
+        const secondElement = trElement.children[1];
+
+        if (secondElement.tagName === 'TD' && secondElement.id !== 'sum') {
+            const price = Number(secondElement.textContent);
+            sum += price;
+        }
+    }
+
+    resultElement.textContent = sum;
+}
+
+
