869a47fcc6eede572c6d1af78c1a047c6feff933
Borislav Ivanov <slambeca@abv.bg>
2024-07-26 18:28:24 +0300

    Add files via upload

---
+++ 5.1 DOM Introduction - Lab/ShowMore.js
@@ -0,0 +1,7 @@
+function showText() {
+    const moreLinkElement = document.getElementById('more');
+    const spanElement = document.getElementById('text');
+
+    moreLinkElement.style.display = 'none';
+    spanElement.style.display = 'inline';
+}
