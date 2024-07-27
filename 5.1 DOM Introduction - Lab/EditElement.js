869b0d5eb397474a403ec2c75321ce110f92303b
Borislav Ivanov <slambeca@abv.bg>
2024-07-27 12:37:50 +0300

    Second resolution added

--- 5.1 DOM Introduction - Lab/EditElement.js
+++ 5.1 DOM Introduction - Lab/EditElement.js
@@ -1,3 +1,8 @@
 function edit(element, match, replacer) {
     element.textContent = element.textContent.replace(new RegExp(match, 'g'), replacer);
 }
+
+// Variant 2
+function edit(element, match, replacer) {
+    element.textContent = element.textContent.replaceAll(match, replacer);
+}
