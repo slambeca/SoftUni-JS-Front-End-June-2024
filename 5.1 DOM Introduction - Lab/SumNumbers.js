50230e0d09f059d496d7dbf1916aa263825cd2cf
Borislav Ivanov <slambeca@abv.bg>
2024-07-29 14:58:05 +0300

    Second resolution added

--- 5.1 DOM Introduction - Lab/SumNumbers.js
+++ 5.1 DOM Introduction - Lab/SumNumbers.js
@@ -1,5 +1,4 @@
 function calc() {
-    debugger;
     const num1Element = document.getElementById('num1');
     const num2Element = document.getElementById('num2');
     const sumElement = document.getElementById('sum');
@@ -7,4 +6,16 @@ function calc() {
     const sumElements = parseInt(num1Element.value) + parseInt(num2Element.value);
 
     sumElement.value = sumElements;
+}
+
+
+// Variant 2
+function calc() {
+    const firstElement = document.getElementById('num1');
+    const secondElement = document.getElementById('num2');
+    const sumElement = document.getElementById('sum');
+
+    const sumOfElements = Number(firstElement.value) + Number(secondElement.value);
+
+    sumElement.value = sumOfElements;
 }
