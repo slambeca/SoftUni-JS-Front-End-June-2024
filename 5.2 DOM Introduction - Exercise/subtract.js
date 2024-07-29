d5332111e79e60ed3eab75eb7e270337415fd3b7
Borislav Ivanov <slambeca@abv.bg>
2024-07-29 15:30:24 +0300

    Second shorter resolution added

--- 5.2 DOM Introduction - Exercise/subtract.js
+++ 5.2 DOM Introduction - Exercise/subtract.js
@@ -1,8 +1,16 @@
 function subtract() {
-    
+
     const firstValue = document.getElementById('firstNumber').value;
     const secondValue = document.getElementById('secondNumber').value;
     const resultField = document.getElementById('result');
 
     resultField.textContent = Number(firstValue) - Number(secondValue);
+}
+
+// Variant 2
+function subtract() {
+
+    const [firstElement, secondElement] = document.querySelectorAll('input[type="text"]');
+
+    document.getElementById('result').textContent = Number(firstElement.value) - Number(secondElement.value);
 }
