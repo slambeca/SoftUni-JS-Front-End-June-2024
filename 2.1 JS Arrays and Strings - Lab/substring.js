2c180ce3bd042469361745a6ead99f20599cf676
Borislav Ivanov <slambeca@abv.bg>
2024-08-08 16:46:31 +0300

    Second shorter resolution added

--- 2.1 JS Arrays and Strings - Lab/substring.js
+++ 2.1 JS Arrays and Strings - Lab/substring.js
@@ -4,3 +4,9 @@ function substringFromAString(string, startIndex, count) {
 
 substringFromAString('ASentence', 1, 8);
 substringFromAString('SkipWord', 4, 7);
+
+// Variant 2
+const substringFromAString = (s, i, c) => console.log(s.substr(i, c));
+
+substringFromAString('ASentence', 1, 8);
+substringFromAString('SkipWord', 4, 7);
