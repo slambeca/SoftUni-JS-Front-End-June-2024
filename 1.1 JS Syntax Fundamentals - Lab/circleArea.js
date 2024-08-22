940909d20c1c78c5b84178ec4f099883286daa6d
Borislav Ivanov <slambeca@abv.bg>
2024-08-22 21:39:52 +0300

    Update circleArea.js

--- 1.1 JS Syntax Fundamentals - Lab/circleArea.js
+++ 1.1 JS Syntax Fundamentals - Lab/circleArea.js
@@ -13,3 +13,20 @@ function circleArea(input) {
 
 circleArea(5);
 circleArea('name');
+
+// Variant 2
+function calculateCircleArea(value) {
+    let area;
+
+    const valueType = typeof value;
+
+    if (valueType === 'number') {
+        area = Math.PI * value ** 2;
+        console.log(area.toFixed(2));
+    } else {
+        console.log(`Unable to compute the circle area because the provided input is of type ${valueType}.`);
+    }
+}
+
+calculateCircleArea(5);
+calculateCircleArea('name');
