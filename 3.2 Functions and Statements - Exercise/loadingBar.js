ee8bf55593c0bb263254f7b24bf7980907858f27
Borislav Ivanov <slambeca@abv.bg>
2024-08-25 00:43:10 +0300

    Shorter second variant added

--- 3.2 Functions and Statements - Exercise/loadingBar.js
+++ 3.2 Functions and Statements - Exercise/loadingBar.js
@@ -19,3 +19,13 @@ function loadingBar(number=Number) {
 loadingBar(30);
 loadingBar(50);
 loadingBar(100);
+
+// Variant 2
+function loadingBar(n) {
+    const bar = '%'.repeat(n / 10) + '.'.repeat(10 - n / 10);
+    console.log(`${n}% [${bar}] ${n === 100 ? 'Complete!' : 'Still loading...'}`);
+}
+
+loadingBar(30);
+loadingBar(50);
+loadingBar(100);
